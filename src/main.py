37002d2f7ef3434634181fbc201f16d29a332e05
Riku Rauhala <riku@rauhala.io>
2023-01-31 09:38:40 +0200

    Create function for fetching OSM map tiles

--- src/main.py
+++ src/main.py
@@ -47,6 +47,19 @@ def get_rev_number():
     return f"Berry Picker Tracker Server\n{details}"
 
 
+@app.get("/osmapi/{z}/{y}/{x}")
+def get_nls_tile(z, y, x):
+    url = "http://a.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png".format(
+        z=z, y=y, x=x
+    )
+    response = requests.get(url, stream=True)
+    if response.status_code == 200:
+        return Response(
+            content=response.content, media_type="image/png", status_code=200
+        )
+    raise HTTPException(status_code=404, detail="Image not found.")
+
+
 @app.get("/nlsapi/{z}/{y}/{x}")
 def get_nls_tile(z, y, x):
     url = "https://avoin-karttakuva.maanmittauslaitos.fi/avoin/wmts/1.0.0/maastokartta/default/WGS84_Pseudo-Mercator/{z}/{y}/{x}.png".format(
